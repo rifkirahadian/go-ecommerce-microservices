23169b5eabe6660d63f476bb1ec9b495be651085
Rifki Rahadian <rrahadian94@gmail.com>
2024-06-07 08:21:51 +0700

    chore: product-service: auth middleware

---
+++ product-service/src/clients/user-service.go
@@ -0,0 +1,47 @@
+package clients
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"shop/product-service/src/dtos"
+)
+
+var (
+	authServiceUrl = "http://localhost:8081"
+)
+
+func ValidateToken(token string) (bool, dtos.User, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/auth/user", authServiceUrl), nil)
+	if err != nil {
+		return false, dtos.User{}, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, dtos.User{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, dtos.User{}, fmt.Errorf("validation service returned status: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, dtos.User{}, err
+	}
+
+	var result struct {
+		Data dtos.User `json:"data"`
+	}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return false, dtos.User{}, err
+	}
+
+	return true, result.Data, nil
+}
